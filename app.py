0f2deef750bc5b33d19da666351f554a874a2432
VamsiMohanRamin <vmr286@nyu.edu>
2023-03-18 18:35:28 +0000

    Added dynamic data to html files and created api route.

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Machine Learning Engineer',
+    'location': 'New York',
+    'salary': '$ 100,000'
+}, {
+    'id': 2,
+    'title': 'Software Engineer',
+    'location': 'Boston',
+    'salary': '$ 85,000'
+}, {
+    'id': 3,
+    'title': 'Data Analyst',
+    'location': 'Seattle',
+    'salary': '$ 175,000'
+}]
 
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
